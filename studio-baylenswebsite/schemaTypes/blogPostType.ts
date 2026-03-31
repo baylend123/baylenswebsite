e262eb4f6f1ce1dec5df4f3ecec38d6ad6057d73
Baylen Doss <baylendoss12@gmail.com>
2026-03-31 17:26:33 -0500

    First BlogPost Type Created in sanity

---
+++ studio-baylenswebsite/schemaTypes/blogPostType.ts
@@ -0,0 +1,51 @@
+import {defineField, defineType} from 'sanity'
+
+export const blogPostType = defineType({
+  name: 'BlogPost',
+  title: 'BlogPost',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'heroImage',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'readingTime',
+      type: 'number',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'author',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'authorImage',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
