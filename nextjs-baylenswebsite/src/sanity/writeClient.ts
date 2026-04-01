f5bbd1cfc1fa23a307737d897df3b84b6fbde4be
Baylen Doss <baylendoss12@gmail.com>
2026-04-01 14:16:03 -0500

    adding project ID to env vard

--- nextjs-baylenswebsite/src/sanity/writeClient.ts
+++ nextjs-baylenswebsite/src/sanity/writeClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const writeClient = createClient({
-  projectId: "cqsj8toq",
-  dataset: "production",
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   apiVersion: "2026-03-01",
   useCdn: false,
   token: process.env.SANITY_API_WRITE_TOKEN,
